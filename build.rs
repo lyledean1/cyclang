9241b1b21c780a5b1124192a2fcf75b39fa95670
Lyle Dean <dean@lyle.dev>
2023-09-05 22:12:55 +0100

    Add Build Script for LLVM (#46)

---
+++ build.rs
@@ -0,0 +1,46 @@
+use std::process::Command;
+use std::env;
+use std::path::Path;
+
+fn main() {
+    let which_output = Command::new("which")
+        .arg("llvm-config")
+        .output()
+        .expect("Failed to execute `which`. Make sure it's installed and available in PATH.");
+
+    if !which_output.status.success() {
+        panic!("Could not find `llvm-config`. Make sure llvm is installed.");
+    }
+
+    let which_str = String::from_utf8_lossy(&which_output.stdout);
+    let llvm_config_path = which_str.trim();
+
+    let llvm_config_output = Command::new(llvm_config_path)
+        .arg("--version")
+        .output()
+        .expect("Failed to execute llvm-config.");
+
+    if !llvm_config_output.status.success() {
+        panic!("llvm-config execution failed");
+    }
+
+    let version_str = String::from_utf8_lossy(&llvm_config_output.stdout);
+    let version_str = version_str.trim();
+
+    println!("Found LLVM version: {}", version_str);
+
+    if !version_str.starts_with("16.0") {
+        panic!("Unsupported LLVM version: {}. LLVM 16.0.x is required.", version_str);
+    }
+
+    let llvm_dir = Path::new(llvm_config_path)
+        .parent()
+        .expect("Failed to get parent directory of llvm-config")
+        .parent()
+        .expect("Failed to get LLVM directory");
+
+    let llvm_dir_str = llvm_dir.to_str().expect("Failed to convert path to string");
+
+    env::set_var("LLVM_SYS_160_PREFIX", llvm_dir_str);
+    println!("cargo:rerun-if-changed=build.rs");
+}
