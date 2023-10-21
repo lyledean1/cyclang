46de01228580789f87b8314852f8474b4cb7341f
Lyle Dean <dean@lyle.dev>
2023-10-22 00:17:23 +0100

    Improve REPL

--- src/repl.rs
+++ src/repl.rs
@@ -4,30 +4,32 @@ use crate::parser;
 use rustyline::error::ReadlineError;
 use rustyline::DefaultEditor;
 use text_colorizer::*;
-
+use crate::parser::Expression;
+use rustyline::{Cmd, EventHandler, KeyCode, KeyEvent, Modifiers};
 pub fn run() {
     let version: &str = env!("CARGO_PKG_VERSION");
 
-    println!("{} version: {}", "#".bold(), version.italic());
-    println!();
-
+    println!("{} {}", "cyclang".italic(), version.italic());
     let mut rl = DefaultEditor::new().unwrap();
-    if rl.load_history("history.txt").is_err() {
-        println!();
-    }
+    rl.bind_sequence(
+        KeyEvent(KeyCode::Down, Modifiers::SHIFT),
+        EventHandler::Simple(Cmd::Newline),
+    );
+
     loop {
         let line = rl.readline(">> ");
         match line {
             Ok(input) => match input.trim() {
                 "exit()" => break,
                 _ => {
-                    let _ = rl.add_history_entry(input.as_str());
-                    match parse_and_compile(input.to_string()) {
+                    match parse_and_compile(input.to_string(), &mut rl) {
                         Ok(output) => {
-                            println!("{}", output)
+                            if !output.is_empty() {
+                                println!("{:?}", output)
+                            }
                         }
                         Err(e) => {
-                            eprintln!("{}", e);
+                            println!("{}", e.to_string().red());
                         }
                     }
                 }
@@ -47,8 +49,21 @@ pub fn run() {
     }
 }
 
-fn parse_and_compile(input: String) -> Result<String, CycloError> {
-    let exprs = parser::parse_cyclo_program(&input)?;
-    let output = compiler::compile(exprs, true)?;
+fn parse_and_compile(input: String, rl: &mut DefaultEditor) -> Result<String, CycloError> {
+    let joined_history = rl.history()
+        .iter()
+        .map(|s| &**s)  // Convert &String to &str
+        .collect::<Vec<&str>>()
+        .join("\n");
+
+    let final_string = format!("{}{}", joined_history, input);
+    let exprs = parser::parse_cyclo_program(&final_string)?;
+    let output = compiler::compile(exprs.clone(), true)?;
+
+    for expr in parser::parse_cyclo_program(&input)? {
+        if let Expression::LetStmt(_, _) | Expression::FuncStmt(_, _, _, _) = expr {
+            let _ = rl.add_history_entry(input.as_str());
+        }
+    }
     Ok(output)
 }
