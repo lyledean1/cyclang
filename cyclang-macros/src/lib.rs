8096b2ffcfb7da0d93c987c0da80f045668403e3
Lyle Dean <dean@lyle.dev>
2024-03-24 14:39:05 +0000

    Remove debug macro (#80)

--- cyclang-macros/src/lib.rs
+++ cyclang-macros/src/lib.rs
@@ -2,12 +2,10 @@ extern crate proc_macro;
 mod arithmetic;
 mod base;
 mod comparison;
-mod debug;
 
 use crate::arithmetic::generate_arithmetic_derive;
 use crate::base::generate_base_derive;
 use crate::comparison::generate_comparison_derive;
-use crate::debug::generate_debug_derive;
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(ArithmeticMacro)]
@@ -18,11 +16,6 @@ pub fn export_arithmetic_derive(input: TokenStream) -> TokenStream {
 #[proc_macro_derive(ComparisonMacro)]
 pub fn export_comparison_derive(input: TokenStream) -> TokenStream {
     generate_comparison_derive(input)
-}
-
-#[proc_macro_derive(DebugMacro)]
-pub fn export_debug_derive(input: TokenStream) -> TokenStream {
-    generate_debug_derive(input)
 }
 
 #[proc_macro_derive(BaseMacro, attributes(base_type))]
