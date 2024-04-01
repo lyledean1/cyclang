a270038adc8eb58af2e1bd2ceee7ac605c368ecd
Lyle Dean <dean@lyle.dev>
2024-04-01 21:56:00 +0100

    Add Zig LLVM Stdlib

---
+++ crates/cyclang-stdlib/src/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+const testing = std.testing;
+
+fn exportBuiltinFn(comptime func: anytype, comptime func_name: []const u8) void {
+    @export(func, .{ .name = "cyclang_stdlib." ++ func_name, .linkage = .Strong });
+}
+
+comptime {
+    exportBuiltinFn(boolToStrZig, "boolToStr");
+}
+
+export fn boolToStrZig(value: bool) [*:0]const u8 {
+    if (value) {
+        return "true";
+    } else {
+        return "false";
+    }
+}
+
+export fn add(a: i32, b: i32) i32 {
+    return a + b;
+}
+
+test "basic add functionality" {
+    try testing.expect(add(3, 7) == 10);
+}
